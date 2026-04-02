b78e5dc134073c6375900c8f0a060c1fef11700a
Axel Napolitano <github.2025@skjt.de>
2026-04-02 12:35:44 +0200

    feat(ui): enhance arrow drawing and progress visualization in SongPainter

--- src/apps/sequencer/ui/painters/SongPainter.cpp
+++ src/apps/sequencer/ui/painters/SongPainter.cpp
@@ -1,3 +1,4 @@
+#include "Config.h"
 #include "SongPainter.h"
 
 void SongPainter::drawArrowDown(Canvas &canvas, int x, int y, int w) {
@@ -17,15 +18,19 @@ void SongPainter::drawArrowUp(Canvas &canvas, int x, int y, int w) {
 void SongPainter::drawArrowRight(Canvas &canvas, int x, int y, int w, int h) {
     x += w / 2;
     y += h / 2;
-    canvas.hline(x, y, 2);
-    canvas.point(x, y - 1);
-    canvas.point(x, y + 1);
+
+    canvas.setFont(Font::Tiny);
+    canvas.drawText(x - 2, y + 2, GLYPH_TINY_SMALL_ARROW_RIGHT);
+
+    canvas.setBlendMode(BlendMode::Add);
+    canvas.setColor(UI_COLOR_INACTIVE);
+    canvas.fillRect(x - 2, y - 3, CONFIG_LCD_WIDTH - x + 2, 7);
 }
 
 void SongPainter::drawProgress(Canvas &canvas, int x, int y, int w, int h, float progress) {
     int pw = w * progress;
-    canvas.setColor(0x7);
+    canvas.setColor(UI_COLOR_DIM_MORE);
     canvas.fillRect(x + pw, y, w - pw, h);
-    canvas.setColor(0xf);
+    canvas.setColor(UI_COLOR_ACTIVE);
     canvas.fillRect(x, y, pw, h);
 }
